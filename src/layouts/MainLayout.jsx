f0f44b68f3ebd9b214e97acb2a2b1c2ce970f986
Your Name <197651399+koti16@users.noreply.github.com>
2026-07-25 17:31:00 +0530

    Create landing page with Tailwind and navigation

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
 
 function MainLayout() {
   return (
     <>
-      <header>
-        Navbar
-      </header>
+      <Navbar />
 
       <main>
         <Outlet />
       </main>
 
-      <footer>
-        Footer
-      </footer>
+      <footer/>
+        
     </>
   );
 }
